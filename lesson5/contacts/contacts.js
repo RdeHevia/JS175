61b5de6491725d84a2023b258617c50102625870
Raul de Hevia <raul.dehevia@gmail.com>
2021-03-23 13:27:27 -0700

    v1.3

--- lesson5/contacts/contacts.js
+++ lesson5/contacts/contacts.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const {body, validationResult} = require("express-validator");
 
 let contactData = [
   {
@@ -41,91 +42,17 @@ const sortContacts = contacts => {
     }
   });
 };
-/*
-trim leading & trailing spaces
-all input fields are required
-first & last name:
-  max length = 25 chars
-  alphabetic chars
-  full name must be unique
-phone number: match ###-###-#### format
-ALGORITHM:
-  1. Remove leading and trailing spaces
-  2. Check input is not missing
-    - missing -> display error
-    - not missing -> continue
-  3. check first name is correct
-  4. check last name is correct
-  5. check full name is unique
-  6. check phone number is in the correct format
-*/
 
-function trimLeadingAndEndSpaces(req, res, next) {
-  res.locals.firstName = req.body.firstName.trim();
-  res.locals.lastName = req.body.lastName.trim();
-  res.locals.phoneNumber = req.body.phoneNumber.trim();
-
-  next();
-}
-
-function nameIsCorrect(name) {
-  return name.match(/^[a-z]+$/i) && name.length <= 25;
-}
-
-function firstNameValidation(req, res, next) {
-  let firstName = res.locals.firstName;
-  if (!nameIsCorrect(firstName)) {
-    res.locals.errorMessages.push(
-      "First name must use alphabetic characters (25 max)."
-    );
-  }
-
-  next();
-}
-
-function lastNameValidation(req, res, next) {
-  let lastName = res.locals.lastName;
-  if (!nameIsCorrect(lastName)) {
-    res.locals.errorMessages.push(
-      "Last name must use alphabetic characters (25 max)."
-    );
-  }
-
-  next();
-}
-
-function fullNameIsUniqueValidation(req, res, next) {
-  let firstName = res.locals.firstName;
-  let lastName = res.locals.lastName;
-  let fullName = `${firstName} ${lastName}`;
-
-  if (contactsFullNames(contactData).includes(fullName)) {
-    res.locals.errorMessages.push(`The contact ${fullName} already exists`);
-  }
-
-  next();
-}
-
-function contactsFullNames(contacts) {
-  let fullNames = [];
-  contacts.forEach(contact => {
-    fullNames.push(`${contact.firstName} ${contact.lastName}`);
-  });
-  console.log(fullNames);
-  return fullNames;
-}
-
-function phoneValidation(req, res, next) {
-  let phoneFormat = /[0-9]{3}-[0-9]{3}-[0-9]{4}/;
-  let phone = res.locals.phoneNumber;
-
-  if (!phone.match(phoneFormat)) {
-    res.locals.errorMessages.push(
-      `The phone number must have the ###-###-#### format`
-    );
-  }
-
-  next();
+function validateName (name, whichName) {
+  return body(name)
+    .trim()
+    .isLength({min: 1})
+    .withMessage(`${whichName} name is required`)
+    .bail()
+    .isLength({max: 25})
+    .withMessage(`${whichName} name is too long. Maximum length is 25 characters`)
+    .isAlpha()
+    .withMessage(`${whichName} name contains invalid characters. The name must be alphabetic.`);
 }
 
 app.set("views", "./views");
@@ -150,60 +77,38 @@ app.get("/contacts/new", (req, res) => {
 });
 
 app.post("/contacts/new",
-  (req, res, next) => trimLeadingAndEndSpaces(req, res, next),
+  [
+    validateName("firstName", "First"),
+    validateName("lastName", "Last"),
+
+    body("phoneNumber")
+      .trim()
+      .isLength({min: 1})
+      .withMessage("Phone number is required.")
+      .bail()
+      .matches(/^\d{3}-\d{3}-\d{4}$/)
+      .withMessage("Invalid phone number format. Use ###-###-####")
+  ],
 
   (req, res, next) => {
-    res.locals.errorMessages = [];
-    next();
-  },
-
-  (req, res, next) => {
-    if (res.locals.firstName.length === 0) {
-      res.locals.errorMessages.push("First name is required.");
-    }
-
-    next();
-  },
-
-  (req, res, next) => {
-    if (res.locals.lastName.length === 0) {
-      res.locals.errorMessages.push("Last name is required.");
-    }
-
-    next();
-  },
-
-  (req, res, next) => {
-    if (res.locals.phoneNumber.length === 0) {
-      res.locals.errorMessages.push("Phone number is required.");
-    }
-
-    next();
-  },
-
-  (req, res, next) => firstNameValidation(req, res, next),
-  (req, res, next) => lastNameValidation(req, res, next),
-  (req, res, next) => fullNameIsUniqueValidation(req, res, next),
-  (req, res, next) => phoneValidation(req, res, next),
-
-  (req, res, next) => {
-    if (res.locals.errorMessages.length > 0) {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
       res.render("new-contact", {
-        errorMessages: res.locals.errorMessages,
-        firstName: res.locals.firstName,
-        lastName: res.locals.lastName,
-        phoneNumber: res.locals.phoneNumber,
+        errorMessages: errors.array().map(error => error.msg),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phoneNumber: req.body.phoneNumber,
       });
     } else {
       next();
     }
   },
 
-  (req, res, next) => {
+  (req, res) => {
     contactData.push({
-      firstName: res.locals.firstName,
-      lastName: res.locals.lastName,
-      phoneNumber: res.locals.phoneNumber,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phoneNumber: req.body.phoneNumber,
     });
     res.redirect("/contacts");
   }
