2f26bd0fd9ac2351f8578d0aff840bbc2b019caf
Raul de Hevia <raul.dehevia@gmail.com>
2021-03-23 17:33:06 -0700

    v1.5.

--- lesson5/contacts/contacts.js
+++ lesson5/contacts/contacts.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const {body, validationResult} = require("express-validator");
 const session = require("express-session");
 const store = require("connect-loki");
+const flash = require("express-flash");
 
 const app = express();
 const LokiStore = store(session);
@@ -83,11 +84,19 @@ app.use(session({
   store: new LokiStore({}),
 }));
 
+app.use(flash());
+
 app.use((req, res, next) => {
   if (!("contactData" in req.session)) {
     req.session.contactData = clone (contactData);
   }
 
+  next();
+});
+
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
   next();
 });
 
@@ -122,8 +131,10 @@ app.post("/contacts/new",
   (req, res, next) => {
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
+      errors.array().forEach(error => req.flash("error", error.msg));
+
       res.render("new-contact", {
-        errorMessages: errors.array().map(error => error.msg),
+        flash: req.flash(),
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         phoneNumber: req.body.phoneNumber,
@@ -139,6 +150,8 @@ app.post("/contacts/new",
       lastName: req.body.lastName,
       phoneNumber: req.body.phoneNumber,
     });
+
+    req.flash("success", "New contact added to list!");
     res.redirect("/contacts");
   }
 );
